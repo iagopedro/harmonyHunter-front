e6d22da2209d7dd2bef38c7a671c1115e3b8922c
iagopedro <iagopedroltd19@gmail.com>
2025-05-15 20:27:10 -0300

    create Info component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { InfoComponent } from './components/info/info.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
+    {path: 'info', component: InfoComponent},
 ];
